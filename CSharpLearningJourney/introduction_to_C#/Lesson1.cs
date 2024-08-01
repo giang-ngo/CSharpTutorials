7a94f92cd3fdca67119fd1645cc0b7e18978282c
giang-ngo <giangngo.py@gmail.com>
2024-08-01 18:39:51 +0700

    Getting Started with C#

---
+++ CSharpLearningJourney/introduction_to_C#/Lesson1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLearningJourney.introduction_to_C_
+{
+    class Lesson1
+    {
+        static void Main()
+        {
+            var originalFg = Console.ForegroundColor;
+            var originalBg = Console.BackgroundColor;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine("Hello CSharp!");
+            Console.WriteLine("I'm Giang Ngo");
+            Console.WriteLine("I'm 21");
+            Console.ForegroundColor = originalFg;
+            Console.BackgroundColor = originalBg;
+        }
+    }
+}
